8f952b2450b980d1b077ec140c5eee1f6c4e0ce8
Ayoub Rjiba <rjibaayoub@gmail.com>
2026-06-02 11:54:18 +0100

    fix bug

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,22 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-resource "aws_security_group" "web_sg" {
+resource "tls_private_key" "ec2_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "ec2_key" {
+  key_name   = "ec2-key"
+  public_key = tls_private_key.ec2_key.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  filename        = "${path.module}/ec2-key.pem"
+  content         = tls_private_key.ec2_key.private_key_pem
+  file_permission = "0400"
+}
+resource "aws_security_group" "web_sg2" {
   name = "web-sg"
 
   ingress {
