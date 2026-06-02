70bdda30d2f7066d231f469cea11dcfdf9044e15
Ayoub Rjiba <rjibaayoub@gmail.com>
2026-06-02 11:18:22 +0100

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name = "web-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = file("${path.module}/script.sh")
+
+  tags = {
+    Name = "devops-web"
+  }
+}
---
+++ terraform/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+dnf update -y
+dnf install nginx -y
+
+systemctl enable nginx
+systemctl start nginx
