2871f22a9ddd6e0f32e88378f20c1858e1bdcce7
Ayoub Rjiba <rjibaayoub@gmail.com>
2026-06-02 12:38:20 +0100

    fix bug

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,23 +21,23 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-resource "tls_private_key" "ec2_key3" {
+resource "tls_private_key" "ec2_key4" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-resource "aws_key_pair" "ec2_key3" {
+resource "aws_key_pair" "ec2_key4" {
   key_name   = "ec2-key3"
-  public_key = tls_private_key.ec2_key3.public_key_openssh
+  public_key = tls_private_key.ec2_key4.public_key_openssh
 }
 
 resource "local_file" "private_key" {
   filename        = "${path.module}/ec2-key.pem"
-  content         = tls_private_key.ec2_key3.private_key_pem
+  content         = tls_private_key.ec2_key4.private_key_pem
   file_permission = "0400"
 }
-resource "aws_security_group" "web_sg5" {
-  name = "web_sg5"
+resource "aws_security_group" "web_sg6" {
+  name = "web_sg6"
 
   ingress {
     from_port   = 22
@@ -64,8 +64,8 @@ resource "aws_security_group" "web_sg5" {
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.web_sg5.id]
-  key_name = aws_key_pair.ec2_key3.key_name
+  vpc_security_group_ids = [aws_security_group.web_sg6.id]
+  key_name = aws_key_pair.ec2_key4.key_name
 
   user_data = file("${path.module}/script.sh")
 
